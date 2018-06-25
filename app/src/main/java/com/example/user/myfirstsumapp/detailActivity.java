7207f7c60017153feac136ec09b35aaffada9953
nisimth <34004093+nisimth@users.noreply.github.com>
2018-06-25 13:34:20 +0300

    test

--- app/src/main/java/com/example/user/myfirstsumapp/detailActivity.java
+++ app/src/main/java/com/example/user/myfirstsumapp/detailActivity.java
@@ -3,13 +3,28 @@ package com.example.user.myfirstsumapp;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class detailActivity extends AppCompatActivity {
+
+    // test
+    //
+    //
+    //
+    //
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
